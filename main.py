77d6b739351154931955a465eb30f2e75d0f4907
Elijah Mbaabu <elijahmbaabu1@gmail.com>
2022-01-20 16:36:51 +0300

    Second commit

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ from csv import writer
 url = 'https://www.pararius.com/apartments/amsterdam-duivendrecht?ac=1'
 
 page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.content, 'lxml')
 lists = soup.find_all('section', class_="listing-search-item")
+
 with open('rentals.csv', 'w', encoding='utf8', newline='') as f:
     The_writer = writer(f)
-    Header = ['Title', 'Location', 'Price', 'Area', 'Rooms', 'Construction Period']
+    Header = ['Title', 'Location', 'Price', 'Area', 'Rooms']
     The_writer.writerow(Header)
 
-for list in lists:
-    title = list.find('a', class_="listing-search-item__link--title").text
-    location = list.find('div', class_="listing-search-item__location").text
-    price = list.find('div', class_="listing-search-item__price").text
-    area = list.find('li', class_="illustrated-features__item--surface-area").text
-    rooms = list.find('li', class_="illustrated-features__item--number-of-rooms").text
-    construction = list.find('li', class_="illustrated-features__item--construction-period").text
-    info = [title, location, price, area, rooms, construction]
-    The_writer.writerow(Header)
+    for list in lists:
+        title = list.find('a', class_="listing-search-item__link--title").text.replace('\n', ' ')
+        location = list.find('div', class_="listing-search-item__location").text.replace('\n', ' ')
+        price = list.find('div', class_="listing-search-item__price").text.replace('\n', ' ')
+        area = list.find('li', class_="illustrated-features__item--surface-area").text.replace('\n', ' ')
+        rooms = list.find('li', class_="illustrated-features__item--number-of-rooms").text.replace('\n', ' ')
+        info = [title, location, price, area, rooms]
+        The_writer.writerow(info)
+
 
